d5bce37667227ce2769bbc863f3c8846d62303b7
CASL0 <gin.0046.822@gmail.com>
2022-04-15 23:58:33 +0900

    アプリパッケージのCardのコンポーネントを作成

---
+++ app/src/main/java/jp/co/casl0/android/simpleappblocker/ui/molecules/AppPackageCard.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2022 CASL0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jp.co.casl0.android.simpleappblocker.ui.molecules
+
+import android.content.res.Configuration
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import jp.co.casl0.android.simpleappblocker.ui.atoms.HeaderText
+import jp.co.casl0.android.simpleappblocker.ui.atoms.IconImage
+import jp.co.casl0.android.simpleappblocker.ui.atoms.SubText
+import jp.co.casl0.android.simpleappblocker.ui.theme.ApplicationTheme
+
+@Composable
+fun AppPackageCard(icon: Drawable, appName: String, packageName: String) {
+    Surface(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp).height(IntrinsicSize.Max)
+    ) {
+        Row(
+            modifier = Modifier.background(MaterialTheme.colors.background)
+                .fillMaxWidth().padding(8.dp)
+        ) {
+            IconImage(icon)
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                HeaderText(appName)
+                SubText(packageName)
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(name = "Dark Mode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewAppPackageCard() {
+    ApplicationTheme {
+        val packageName = LocalContext.current.packageName
+        val icon = LocalContext.current.packageManager.getApplicationIcon(packageName)
+        AppPackageCard(icon, "SimpleAppBlocker", packageName)
+    }
+}
