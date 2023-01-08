14a28e0bf0f56bed654e3ea2ecd410fc60d6a067
CASL0 <gin.0046.822@gmail.com>
2023-01-08 15:50:00 +0900

    不要になったファイルを削除

--- bootstrap.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001
-
-echo ＊＊＊＊＊ PcapPlusPlusのライブラリを取得しています ＊＊＊＊＊
-curl -SL --output pcapplusplus-android.tar.gz https://github.com/seladb/PcapPlusPlus/releases/download/v21.11/pcapplusplus-21.11-android.tar.gz
-tar -xf pcapplusplus-android.tar.gz
-del /q pcapplusplus-android.tar.gz
-if not exist .\app\libs\ mkdir .\app\libs\
-move /Y .\pcapplusplus-21.11-android .\app\libs\pcapplusplus
